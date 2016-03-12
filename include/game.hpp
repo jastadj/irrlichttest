598f4d72d61a8b88f51100dc1a7f67aded61666a
jastadj <jastadj@gmail.com>
2016-03-11 18:45:53 -0800

    render and move

--- include/game.hpp
+++ include/game.hpp
@@ -11,6 +11,36 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
+class MyEventReceiver : public IEventReceiver
+{
+public:
+    // This is the one method that we have to implement
+    virtual bool OnEvent(const SEvent& event)
+    {
+        // Remember whether each key is down or up
+        if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+
+        return false;
+    }
+
+    // This is used to check whether a key is being held down
+    virtual bool IsKeyDown(EKEY_CODE keyCode) const
+    {
+        return KeyIsDown[keyCode];
+    }
+
+    MyEventReceiver()
+    {
+        for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+            KeyIsDown[i] = false;
+    }
+
+private:
+    // We use this array to store the current state of each key
+    bool KeyIsDown[KEY_KEY_CODES_COUNT];
+};
+
 class Game
 {
 private:
@@ -20,6 +50,12 @@ private:
     IVideoDriver* driver;
     ISceneManager* smgr;
     IGUIEnvironment* guienv;
+    MyEventReceiver receiver;
+
+    //camera
+    ICameraSceneNode *camera;
+    vector3df cameraPos;
+    vector3df cameraTarget;
 
     //init
     bool initIrrlicht();
@@ -34,4 +70,7 @@ public:
     void start();
 
 };
+
+
+
 #endif // CLASS_GAME
--- source/game.cpp
+++ source/game.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <string>
 
+//irrlicht namespaces
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
 Game::Game()
 {
 
@@ -25,7 +33,7 @@ void Game::start()
 bool Game::initIrrlicht()
 {
     //init device
-    device = createDevice( video::EDT_OPENGL, dimension2d<u32>(640, 480), 16, false, false, false, 0);
+    device = createDevice( video::EDT_OPENGL, dimension2d<u32>(800, 600), 16, false, false, false, &receiver);
     if(!device) {std::cout << "Error creating device.\n";return false;}
     device->setWindowCaption(L"Game");
 
@@ -41,13 +49,92 @@ bool Game::initIrrlicht()
 
 int Game::mainLoop()
 {
+    cameraPos = vector3df(0,0, 30);
+    cameraTarget = vector3df(0,30,0);
+
 
     //add camera to scene
-    smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+    camera = smgr->addCameraSceneNode(0, cameraPos, cameraTarget);
+
+    //add some fog
+    driver->setFog(video::SColor(0,138,125,81), video::EFT_FOG_LINEAR, 250, 1000, .003f, true, false);
+
+    //add light
+    scene::ILightSceneNode* light1 = smgr->addLightSceneNode(0, vector3df(0,0,300), SColorf(0.5f, 0.5f, 0.5f, 0.f), 200.0f);
+    //light1->enableCastShadow(false);
+
+    int msize = 3;
+
+    for(int i = 0; i < msize; i++)
+    {
+        for(int n = 0; n < msize; n++)
+        {
+            f32 cubesize = 10;
+            vector3df cubescale(1,1,1);
+            cubescale = cubescale * 1;
+            vector3df cuberot(0,0,0);
+            vector3df cubepos(cubesize*n, -cubesize*i, 0);
+            f32 cubeid = -1;
+            ISceneNode *mycube = smgr->addCubeSceneNode(cubesize, 0, cubeid, cubepos, cuberot, cubescale);
+            if(mycube)
+            {
+                mycube->setMaterialTexture(0, driver->getTexture("wall.jpg"));
+                mycube->setMaterialFlag(video::EMF_LIGHTING, true);
+                //mycube->setMaterialFlag(video::EMF_LIGHTING, false); //no lighting
+            }
+        }
+    }
+
+    int lastFPS = -1;
+
+    // In order to do framerate independent movement, we have to know
+    // how long it was since the last frame
+    u32 then = device->getTimer()->getTime();
+
+    // This is the movemen speed in units per second.
+    const f32 MOVEMENT_SPEED = 50.f;
 
     //main loop
     while(device->run())
     {
+        // Work out a frame delta time.
+        const u32 now = device->getTimer()->getTime();
+        const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
+        then = now;
+
+        if(receiver.IsKeyDown(KEY_ESCAPE))
+        {
+            device->closeDevice();
+        }
+        else if(receiver.IsKeyDown(KEY_KEY_A))
+        {
+            cameraPos.X += MOVEMENT_SPEED * frameDeltaTime;
+            cameraTarget.X += MOVEMENT_SPEED * frameDeltaTime;
+            camera->setPosition(cameraPos);
+            camera->setTarget(cameraTarget);
+        }
+        else if(receiver.IsKeyDown(KEY_KEY_D))
+        {
+            cameraPos.X -= MOVEMENT_SPEED * frameDeltaTime;
+            cameraTarget.X -= MOVEMENT_SPEED * frameDeltaTime;
+            camera->setPosition(cameraPos);
+            camera->setTarget(cameraTarget);
+        }
+        else if(receiver.IsKeyDown(KEY_KEY_W))
+        {
+            cameraPos.Y += MOVEMENT_SPEED * frameDeltaTime;
+            cameraTarget.Y += MOVEMENT_SPEED * frameDeltaTime;
+            camera->setPosition(cameraPos);
+            camera->setTarget(cameraTarget);
+        }
+        else if(receiver.IsKeyDown(KEY_KEY_S))
+        {
+            cameraPos.Y -= MOVEMENT_SPEED * frameDeltaTime;
+            cameraTarget.Y -= MOVEMENT_SPEED * frameDeltaTime;
+            camera->setPosition(cameraPos);
+            camera->setTarget(cameraTarget);
+        }
+
         //clear scene
         driver->beginScene(true, true, SColor(255,100,101,140));
 
@@ -59,6 +146,20 @@ int Game::mainLoop()
 
         //done and display
         driver->endScene();
+
+        int fps = driver->getFPS();
+
+        if (lastFPS != fps)
+        {
+            core::stringw tmp(L"Game [");
+            tmp += driver->getName();
+            tmp += L"] fps: ";
+            tmp += fps;
+
+            device->setWindowCaption(tmp.c_str());
+            lastFPS = fps;
+        }
+
     }
 
     return 0;
